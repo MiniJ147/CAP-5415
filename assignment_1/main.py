53b1112aef5e0ff85926a42c280c666f80aa044a
Jake Paul <jakep@Jakes-MacBook-Air.local>
2025-09-25 16:52:06 -0400

    Non maximum suppression and other sutff

--- assignment_1/main.py
+++ assignment_1/main.py
@@ -6,8 +6,17 @@
 # show the result of the edge detection from 3 different sigma values
 
 import numpy as np
+import os
 from PIL import Image
 import matplotlib.pyplot as plt
+
+
+TEST_DIR = "./tests"
+OUTPUT_DIR = "./figs"
+
+# (size, sigma)
+SIZE = 5
+SIGMAS = [1,2,3]
 
 def get_gaussian_values(x, sigma):
     eulers_number = np.exp((-0.5) * ((x**2)/(sigma**2))) # -x^2/(2[sigma]^2)
@@ -54,8 +63,74 @@ def conv_x(img, kernel):
     
     return np.array(result)
 
-def main():
-    img_path = "./tests/test-3.png"
+def conv_y(img, kernel):
+    img_t = img.T 
+    conv_t = []
+
+    for i in range(img_t.shape[0]):
+        conv_t.append(convolution_1d(img_t[i, :], kernel))
+
+    return np.array(conv_t).T
+
+def gradient_magnitude(D_X, D_Y):
+    # find smallest common shape
+    min_rows = min(D_X.shape[0], D_Y.shape[0])
+    min_cols = min(D_X.shape[1], D_Y.shape[1])
+    
+    D_X = D_X[:min_rows, :min_cols]
+    D_Y = D_Y[:min_rows, :min_cols]
+
+    return np.sqrt(D_X**2 + D_Y**2)
+
+def calculate_normal_direction_angle(D_X,D_Y):
+    min_rows = min(D_X.shape[0], D_Y.shape[0])
+    min_cols = min(D_X.shape[1], D_Y.shape[1])
+    
+    D_X = D_X[:min_rows, :min_cols]
+    D_Y = D_Y[:min_rows, :min_cols]
+
+    return np.arctan2(D_Y,D_X) % 180
+
+def non_maximum_suppression(M, angles): 
+    # returns [r,q] --> (r = (x,y), q = (x,y))
+    def get_direction(angle):
+        if (0 <= angle < 22.5) or (157.5 <= angle < 180): direction = np.array([[-1,0],[1,0]])
+        elif (22.5 <= angle < 67.5): direction = direction = np.array([[-1,-1],[1,1]])
+        elif (67.5 <= angle < 112.5): direction = np.array([[0,-1],[0,1]])
+        else: direction = np.array([[-1,1],[1,-1]])
+
+        return direction
+
+    H, W = M.shape
+    result = np.zeros((H,W),dtype=np.float32)
+
+    for y in range(1,H-1):
+        for x in range(1,W-1):
+            q, r = get_direction(angle=angles[y][x])
+            q = [x,y] - q
+            r = [x,y] - r
+
+            mx = max(M[q[1],q[0]], M[r[1],r[0]], M[y][x])        
+            if M[y][x] == mx:
+                result[y][x] = M[y][x]
+    return result
+
+def show_img(I, size, img_name, sigma, caption):
+    I_uint8 = I.astype(np.uint8)
+    img = Image.fromarray(I_uint8)
+    caption = caption.replace(' ','-')
+
+    dump_dir = f"{OUTPUT_DIR}/{img_name}/sigma-{sigma}/size-{size}"
+
+    os.makedirs(dump_dir,exist_ok=True)
+    plt.title(f"{caption} sigma = {sigma} | size = {size}")
+
+    plt.imsave(f"{dump_dir}/{caption}.png",img,cmap="gray")
+
+    plt.close()
+
+def cany_edge(img_path, size, sigma):
+    img_name = img_path.split("/")[-1].split(".png")[0]
 
     # #read image
     img = Image.open(img_path).convert("L")
@@ -64,31 +139,52 @@ def main():
     I = np.array(img)
     # I.shape -> (height, width, channels)
 
-    # G = get_gaussian_kernel(size=5,sigma=1)
+    G = get_gaussian_kernel(size=size,sigma=sigma)
 
-    # I_cx = conv_x(I, G)
-    # I_cx_uint8 = I_cx.astype(np.uint8)
-    # img_cx = Image.fromarray(I_cx_uint8)
-    # plt.imshow(img_cx, cmap="gray")
-    # plt.show()
+    # smoothing the image out with the Gaussian distribution 
+    I_smooth = conv_x(conv_y(I,G),G)
 
+    show_img(
+        I=I_smooth,
+        size=size,
+        sigma=sigma,
+        caption=f"Smoothed Image",
+        img_name=img_name)
 
-    G_1der = get_gaussian_1der_kernel(size=5,sigma=2)
+    G_D = get_gaussian_1der_kernel(size=size,sigma=sigma)
 
-    # I_dx = conv_x(I,G_1der)
+    I_D_X = conv_x(I_smooth, G_D)
+    I_D_Y = conv_y(I_smooth, G_D)
 
-    # print(I_dx)
-    print(G_1der)
-    I_cx = conv_x(I, G_1der)
-    I_cx_uint8 = I_cx.astype(np.uint8)
-    img_cx = Image.fromarray(I_cx_uint8)
-    plt.imshow(img_cx)
-    plt.show()
+    I_GRAD_MAG = gradient_magnitude(I_D_X, I_D_Y)
 
+    show_img(
+        I=I_GRAD_MAG,
+        size=size,
+        sigma=sigma,
+        caption=f"Gradient",
+        img_name=img_name)
 
+    angles = calculate_normal_direction_angle(I_D_X, I_D_Y)
+    nms = non_maximum_suppression(M=I_GRAD_MAG, angles=angles)
 
-   
+    show_img(
+        I=nms,
+        size=size,
+        sigma=sigma,
+        caption=f"Non_Maxium_Suppression",
+        img_name=img_name)
 
+def main():
+    imgs = os.listdir(TEST_DIR)
+    for img in imgs:
+        path = f"{TEST_DIR}/{img}"
+        
+        for sigma in SIGMAS: 
+            cany_edge(
+                img_path=path,
+                size=SIZE,
+                sigma=sigma)
 
 if __name__ == "__main__":
     main()
