3e9f359d23829a19f05801b596b09137d520fa85
minij147 <jakepaul628@gmail.com>
2025-12-03 21:20:23 -0500

    final

--- project/segmodel/dataloader.py
+++ project/segmodel/dataloader.py
@@ -15,14 +15,13 @@ Car --> LMV
 """
 import torch
 import os
-import json
 import numpy as np
 from enum import Enum
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 
 class Label(Enum):
-    IGNORE = 255
+    IGNORE = 0
     TRUCK = 1
     BUS = 2
     MOTORCYCLE = 3
@@ -56,6 +55,7 @@ class CarlaDataset(Dataset):
         self.mask_dir = mask_dir
         self.transform = transform
 
+        # sort files via frame number
         sort_key = lambda file: int(file[len("frame_"):-1*len(".png")])
         self.images = sorted(os.listdir(image_dir), key=sort_key)
 
@@ -66,6 +66,7 @@ class CarlaDataset(Dataset):
         img_path = os.path.join(self.image_dir, self.images[idx])
         mask_path = os.path.join(self.mask_dir, self.images[idx])
 
+        # if data is corrupted
         try:
             image = Image.open(img_path).convert("RGB")
         except:
@@ -90,4 +91,62 @@ class CarlaDataset(Dataset):
             matches = np.all(rgb == color, axis=-1)
             mask[matches] = label.value
 
+        return mask
+
+# Traffic Cam
+
+import json
+import cv2
+
+class TrafficCamDataset(Dataset):
+    def __init__(self, image_dir, json_dir, transform=None):
+        self.image_dir = image_dir
+        self.json_dir = json_dir
+        self.transform = transform
+
+
+        # filter out json files from images
+        self.images = list(filter(lambda file_name: file_name.endswith(".jpg"), os.listdir(image_dir)))
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_file = self.images[idx]
+        img_path = os.path.join(self.image_dir, img_file)
+        json_path = os.path.join(self.json_dir, img_file.replace(".jpg", ".json"))
+
+        try:
+            image = Image.open(img_path).convert("RGB")
+        except:
+            print(f"[WARNING] Corrupt image skipped: {img_path}")
+            return self.__getitem__((idx + 1) % len(self))
+
+        width, height = image.size
+
+        mask = self.json_to_mask(json_path, height, width)
+
+        if self.transform:
+            image = self.transform(image)
+        else:
+            image = torch.from_numpy(np.array(image)).permute(2,0,1) / 255.
+
+        return image.float(), torch.from_numpy(mask).long()
+
+    def json_to_mask(self, path, height, width):
+        mask = np.full((height, width), Label.IGNORE.value, dtype=np.uint8)
+
+        with open(path) as f:
+            data = json.load(f)
+
+        for shape in data["shapes"]:
+            label = shape["label"]
+
+            if label not in TRAFFIC_CAM_TO_LABEL:
+                continue
+
+            points = np.array(shape["points"], dtype=np.int32)
+            class_id = TRAFFIC_CAM_TO_LABEL[label].value
+
+            cv2.fillPoly(mask, [points], class_id)
         return mask
--- project/segmodel/main.py
+++ project/segmodel/main.py
@@ -10,27 +10,9 @@ from torch.utils.data import DataLoader
 import dataloader
 from models import DDRNet_23 as DDRNet
 
-def mean_iou(pred, target, num_classes=7):
-    pred = pred.view(-1)
-    target = target.view(-1)
-
-    valid = target != dataloader.Label.IGNORE.value
-    pred = pred[valid]
-    target = target[valid]
-
-    ious = []
-
-    for cls in range(1, num_classes):  # skip ignore and unused 0
-        pred_inds = pred == cls
-        target_inds = target == cls
-
-        intersection = (pred_inds & target_inds).sum().item()
-        union = (pred_inds | target_inds).sum().item()
-
-        if union > 0:
-            ious.append(intersection / union)
-
-    return sum(ious) / len(ious) if len(ious) else 0
+from PIL import Image
+from torchmetrics import JaccardIndex
+import cv2
 
 # config
 BATCH_SIZE = 4
@@ -38,31 +20,49 @@ EPOCHS = 20
 LR = 1e-4
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-print(DEVICE)
+# IOU
+iou = JaccardIndex(task="multiclass", num_classes=7).to(DEVICE)
 
+# transform
 transform = T.Compose([
     T.Resize((512, 512)),
     T.ToTensor(),
 ])
 
-
-dataset = dataloader.CarlaDataset(
+# Synthetic data
+dataset_carla = dataloader.CarlaDataset(
     image_dir="data/carla_captures/Foggy/camera0/raw",
     mask_dir="data/carla_captures/Foggy/camera0/semantic",
     transform=transform,
 )
 
+# real data
+TrafficCAM_DIR = "data/TrafficCAM-fully-annotated/UCF_1651231637.4264545"
+dataset_TrafficCAM = dataloader.TrafficCamDataset(
+    image_dir=TrafficCAM_DIR,
+    json_dir=TrafficCAM_DIR,
+    transform=transform,
+)
+
+# loaders
 train_loader = DataLoader(
-    dataset, 
+    dataset_carla,
     batch_size=BATCH_SIZE, 
     shuffle=True, 
     num_workers=4,
-    pin_memory=True,
 )
 
-model = DDRNet.get_seg_model(False, num_classes=7).to(DEVICE)
+test_loader = DataLoader(
+    dataset_TrafficCAM, 
+    batch_size=BATCH_SIZE, 
+    shuffle=True, 
+    num_workers=4,
+)
 
-criterion = nn.CrossEntropyLoss(ignore_index=dataloader.Label.IGNORE.value)
+# DDRNet model
+model = DDRNet.DualResNet(DDRNet.BasicBlock, [2, 2, 2, 2], num_classes=6, planes=64, spp_planes=128, head_planes=128, augment=False).to(DEVICE)
+
+criterion = nn.CrossEntropyLoss()
 
 optimizer = optim.Adam(model.parameters(), lr=LR)
 
@@ -71,7 +71,7 @@ for epoch in range(EPOCHS):
     running_loss = 0
     running_iou = 0
 
-    pbar = tqdm(train_loader, desc=f"Epoch {epoch+1}/{EPOCHS}")
+    pbar = tqdm(train_loader, desc=f"Train Epoch {epoch+1}/{EPOCHS}")
 
     for images, masks in pbar:
         images = images.to(DEVICE)
@@ -80,6 +80,8 @@ for epoch in range(EPOCHS):
         optimizer.zero_grad()
 
         outputs = model(images)
+
+        # resize
         outputs = torch.nn.functional.interpolate(
             outputs,
             size=masks.shape[-2:],
@@ -93,16 +95,39 @@ for epoch in range(EPOCHS):
         optimizer.step()
 
         preds = outputs.argmax(1)
-        iou = mean_iou(preds, masks)
+    
+        # iou = mean_iou(preds, masks)
+        iou_score = iou(preds, masks)
 
         running_loss += loss.item()
-        running_iou += iou
+        running_iou += iou_score 
 
         pbar.set_postfix({
             "loss": f"{loss.item():.4f}",
-            "IoU": f"{iou:.3f}"
+            "IoU": f"{iou_score:.3f}"
         })
 
     print(f"Epoch {epoch+1}: "
           f"Loss={running_loss/len(train_loader):.4f}, "
           f"IoU={running_iou/len(train_loader):.3f}")
+    
+    # test
+    model.eval()
+    test_iou = 0
+
+    with torch.no_grad():
+        for images, masks in tqdm(test_loader, desc="Testing"):
+            images = images.to(DEVICE)
+            masks  = masks.to(DEVICE)
+
+            outputs = model(images)
+
+            # resize model
+            outputs = torch.nn.functional.interpolate(
+                outputs, size=masks.shape[-2:], mode="bilinear", align_corners=False
+            )
+
+            preds = outputs.argmax(1)
+            test_iou += iou(preds, masks)
+
+    print(f"TEST (TrafficCAM): IoU={test_iou/len(test_loader):.3f}")
